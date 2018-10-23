301ef71a3c9b65dbc0556966481bf65a62005c7e
jdenozi <denozi.j@gmail.com>
2018-10-23 15:51:27 +0200

    Ajout scriptpython tp reporting

--- HMIN113M-SYSTEME/reporting/reporting.py
+++ HMIN113M-SYSTEME/reporting/reporting.py
@@ -12,6 +12,7 @@ date=[]
 #-----------------Dictionnaire----------------------------
 dic={}
 dic_heure={}
+dic_date={}
 #------------------Parsage document-----------------
 
 #Pattern expression reguliere
@@ -27,78 +28,57 @@ for line in last:
         dic[login]=dic.get(login)+1
     else:
         dic[login]=1
-    #Etape 
+    #Etape de création du dictionnaire avec login et temps de connexion par login
     temps=re.sub("\W","",str(re.findall(pattern_heure,line)))
     minute=re.sub("^\d{2}","",temps)
     heure=re.sub("\d{2}$","",temps)
+    date_line=str(re.findall(pattern_date,line))
     
     for i in dic:
         if i==login:
             if temps is not "":
                 if i not in dic_heure:
-                    dic_heure[login]=((int(heure)*60)+int(minute))
+                    dic_heure[date_line]=((int(heure)*60)+int(minute))
+                    
                 else:
-                    dic_heure[login]=dic.get(login)+((int(heure)*60)+int(minute))
+                    dic_heure[date_line]=dic.get(date)+((int(heure)*60)+int(minute))
             else:
                 break
-                        
+    #Etape de création du dictionnaire  avec key de login et date donnant le temps de connexion
+    #date_line=str(re.findall(pattern_date,line))
+    for j in dic_heure:
+        if date_line==j:
+            dic_date[login]=dic_heure
+            
+        else:
+            break
 
-print dic_heure            
-        
 
 
-#for line in line_last:
-#    print line
-#    login=re.sub("\W","", str(re.findall(pattern_log,line)))
-
-#    if login not in liste_login:
-#        liste_login.append(login)
-#    else:
-#        break
-
-#    date.append(re.findall(pattern_date,line))
-#    heure=re.sub("\W","",str(re.findall(pattern_heure,line)))
-#    minute=re.sub("^\d{2}","",heure)
-#    heure_minute=re.sub("\d{2}$","",heure)
-    
-#    print heure
-#    if heure:
-#        heure1=(int(re.sub("\W","",heure))/60)+heure_minute
-#    else:
-#        heure1="none"
-#        if dic_heure.get(login):
-#            dic_heure[login]=dic_heure.get(login)+heure1
-#        else:
-#           dic_heure[login]=heure1
-    
-#    dic[login] =len(date)
-#    for keys in dic:
-#        if keys not in liste_login:
-#            liste_login.append(keys)
-#
-#print dic_heure
-#print dic
-#print liste_login
 def loginConnexion(login):
     print login,"s'est connecté ", dic.get(login),"fois"
     
 def tempsConnexion(login):
-    print login, "s'est connecté", dic_heure.get(login),"minutes"
+    print "les connexions en fonction des dates sont de ",login,dic_date.get(login)
 
-loginConnexion("denozi")
-tempsConnexion("denozi")
+def tempsConnexionDate(login,date):
+    print "la connexion du ",date,"de",login,dic_date.get(login).get(date)
+
+#loginConnexion("denozi")
+#tempsConnexion("denozi")
+tempsConnexionDate("denozi","['Oct 10']")
 # 　　　　　　　　　　　　　　　　　　 ﾍ
 #　　　　　　　　　　　　　　 ﾍ　　　/　|
 #　　　　　　　　　　　　　 / ｜　 /　　|
-#　　　　　　　　　 }YL　 ﾉ　　|　ﾉ 　 　|
-#　　　　　　　　　ﾉ　　ヽﾐ}　F′〉　 ｯ┘
-#　 　 　　　　　　{^^ . -┴┴‐ミ　　ﾐ.._
-#　 　 　　　　　　> ´　　　　　　　　ミ､
-#　　　　　　　 　/　　　　　　　　　　　 ﾐ､
-#　　　　　　　  ﾉ　　p￣ヽ_　　　　　　 　ﾐ､
-#　　　　　rﾍ⌒　　    `ー ′　　　　　　　   ﾐ､
-#　　　　ﾆ{^　　　　　　　　　　　　　　　  ﾐ､
-#　　　　 〈､_　　　＝三二_ー--　　　　　    l
+#　　　　　　　　　    　 ﾉ　　|　ﾉ 　 　|
+#　　      　　　　　　　ﾉ　　ヽﾐ}　F′　 ｯ┘
+#　  　 　    　　　　　{^^ . -┴┴‐ミ   　　ﾐ.._
+#　  　 　　　　　　> ´　　　　　　　    　ミ､
+#　　　　　　　 　/　　　　　　　　　　　    ﾐ､
+#　　　　　　　  ﾉ　　p￣ヽ_　　　　　　     　ﾐ､
+#　　　　　rﾍ⌒　　    `ー ′　　　　　　　       ﾐ､
+#　　　　ﾆ{^　　　　　　　　　　　　　　　      ﾐ､
+#　　　　 〈､_　　　＝三二_ー--　　　　　       l
 #　　　　∠_　　　　　ｰ＝= 二_ｰ
 #　 　／　　 ¨ヾ､
 #　 ﾉﾍ　　　　　　ヽ JulienDenozi
